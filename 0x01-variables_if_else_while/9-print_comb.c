ef14c48a31ec35ee3f0c327183d2d167f99d87bd
Mohamed-Solv <bencheikhmohamedd@gmail.com>
2023-08-11 16:42:08 -0700

    numb with spc

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: A C program
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int ch = '1' - 1;
+
+	while (ch <= '9')
+	{
+		putchar(ch);
+		putchar(44);
+		putchar(32);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
