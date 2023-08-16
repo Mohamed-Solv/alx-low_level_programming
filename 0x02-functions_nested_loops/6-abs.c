024b6657c07e2291fbe89ff385ad8f4fd70398ad
Mohamed-Solv <bencheikhmohamedd@gmail.com>
2023-08-16 14:55:32 -0700

    absolut number t

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -7,17 +7,13 @@
  */
 int _abs(int i);
 {
-	if (i > 0)
+	if (i >= 0)
 	{
 		return (i);
 	}
-	if (i == 0)
-	{
-		return (0);
-	}
 	else
 	{
-		return (i * (-1));
+		return (i * -1);
 	}
 	_putchar('\n');
 }
