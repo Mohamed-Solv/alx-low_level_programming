f23ad36e949836b873e3ae2738217d112fe46696
Mohamed-Solv <bencheikhmohamedd@gmail.com>
2023-08-16 14:01:07 -0700

    lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _islower - check alphabet
+ * @c: the character from ascii
+ * Description: 'prints _putchar too stdout'
+ * Return: always 0
+ */
+int _islower(int c)
+{
+	int c;
+
+	if (c <= 97 && c >= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
