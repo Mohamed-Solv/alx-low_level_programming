381ffaaf7a66d165ead3e562f65e1558e273d00b
Mohamed-Solv <bencheikhmohamedd@gmail.com>
2023-08-18 15:36:41 -0700

    second

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - check alphabet
+ * @a: integer one
+ * @b: integer two
+ * Description: 'check the alphabet'
+ * Return 0
+ */
+int mul(int a, int b)
+{
+	int a, b;
+
+	return (a*b)
+}
