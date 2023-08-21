4f6a2e24fd5261e22579e969f51799dfa0c6c526
Mohamed-Solv <bencheikhmohamedd@gmail.com>
2023-08-21 14:35:25 -0700

    first

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - print alphabet
+ * @n: vari
+ * Description: 'prints _putchar too stdout'
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
