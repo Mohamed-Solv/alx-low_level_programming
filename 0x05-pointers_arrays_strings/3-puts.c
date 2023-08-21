3143e613cb7d88d38c0be76ac1d2faf9d426790a
Mohamed-Solv <bencheikhmohamedd@gmail.com>
2023-08-21 14:57:34 -0700

    first sec third fourth

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - print alphabet
+ * @str: vari
+ * Description: 'prints _putchar too stdout'
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+	_putchar('\n');
+}
