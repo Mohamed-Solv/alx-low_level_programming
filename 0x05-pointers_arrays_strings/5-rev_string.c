8badac6950e96358917961685dc704656f259da4
Mohamed-Solv <bencheikhmohamedd@gmail.com>
2023-08-21 15:15:46 -0700

    first sec third fourth fifth sixth

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - print alphabet
+ * @s: vari
+ * Description: 'prints _putchar too stdout'
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	int l, i;
+	char temp;
+
+	for (i = 0; s[l] != '\0'; ++l)
+		;
+
+	for (i = 0; i < l / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[l - 1 - i];
+		s[l - 1 - i] = temp;
+	}
+}
